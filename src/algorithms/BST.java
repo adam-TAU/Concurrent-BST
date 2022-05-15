4b10689a09d7d365661cc39da6fb19a71b2937a4
Adam-TAU <adamtuby@mail.tau.ac.il>
2022-05-15 19:55:03 +0300

    initalization

---
+++ src/algorithms/BST.java
@@ -0,0 +1,39 @@
+package algorithms;
+
+import main.BSTInterface;
+
+public class BST implements BSTInterface {
+
+    public BST() {
+    }
+
+    public final boolean contains(final int key) {
+    }
+
+    public final boolean insert(final int key) {
+    }
+
+    public final boolean remove(final int key) {
+    }
+
+    // Return your ID #
+    public String getName() {
+        return "XXXXXXXXXX";
+    }
+
+    // Returns size of the tree.
+    public final int size() {
+    // NOTE: Guaranteed to be called without concurrent operations,
+	// so need to be thread-safe.  The method will only be called
+	// once the benchmark completes.
+    }
+
+    // Returns the sum of keys in the tree
+    public final long getKeysum() {
+    // NOTE: Guaranteed to be called without concurrent operations,
+	// so no need to be thread-safe.
+	//
+	// Make sure to sum over a "long" variable or you will get incorrect
+	// results due to integer overflow!
+    }
+}
